dc95557b6485b8ae6e5919beff6a3425ad8a7962
Terry93600 <95472733+Terry93600@users.noreply.github.com>
2024-01-15 18:24:46 +0100

    test back/front

---
+++ front/Pages/Restaurants.jsx
@@ -0,0 +1,14 @@
+import Footer from "../src/components/footer/Footer";
+import Header from "../src/components/header/Header";
+import ListeRestaurant from "../src/components/restaurants/ListeRestaurants";
+
+
+const Restaurants = () => {
+    return <>
+        <Header />
+        <ListeRestaurant />
+        <Footer />
+    </>
+}
+
+export default Restaurants;
