e23cf16c19cbe0f1e5c232ee550e6c2bedd23c2f
Terry93600 <95472733+Terry93600@users.noreply.github.com>
2025-10-21 10:35:12 +0200

    bug Connexion_user_list.jsx

--- frontend/src/components/connexion-user/infoRestaurant/Connexion_user_list.jsx
+++ frontend/src/components/connexion-user/infoRestaurant/Connexion_user_list.jsx
@@ -74,7 +74,7 @@ import { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Connexion_user from "./Connnexion_user.jsx";
 import { getAllRestaurant, getAllEvent } from "../../../../service/api";
-import "./Connnexion_user.jsx.css";
+import "./Connnexion_user.css";
 import { UserContext } from "../../../context/UserProvider";
 
 const Connexion_user_list = () => {
